34a9833095ed6142537d64dc28d72cad1245e19b
Inimfonabasi Udofia <137561774+inimfona@users.noreply.github.com>
2024-07-17 00:40:11 +0100

    Add files via upload

---
+++ happy belly/happy belly/js/index.js
@@ -0,0 +1,32 @@
+let drop =document.querySelector('.hamburger')
+let nav =document.querySelector('.drop-down')
+drop.addEventListener('click',down)
+function down(){
+    nav.classList.toggle('drop-down')
+}
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
