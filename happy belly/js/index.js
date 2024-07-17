a6b27550e71fdc83904ba1724753bcee74416d11
Cletus Kingdom <cletuskingdom@gmail.com>
2024-07-17 19:02:54 +0100

    update

--- happy belly/js/index.js
+++
@@ -1,6 +0,0 @@
-let drop =document.querySelector('.hamburger')
-let nav =document.querySelector('.drop-down')
-drop.addEventListener('click',down)
-function down(){
-    nav.classList.toggle('drop-down')
-}
